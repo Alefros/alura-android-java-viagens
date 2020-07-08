e7d39565fc4eafec397115a95da07299d84f9e6f
= <=>
2020-07-07 23:42:52 -0300

    Aula 4

---
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotes.java
@@ -0,0 +1,14 @@
+package br.com.alura.aluraviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import br.com.alura.aluraviagens.R;
+
+public class ListaPacotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
